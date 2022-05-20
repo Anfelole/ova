712cb50b6e7cfa0315b596d7bd33cf4777eeba69
Anfelole <anfelole11082001@gmail.com>
2022-05-20 12:41:19 -0500

    pagina principal en construccion v1.0

--- src/components/pantallaEntrada.vue
+++ src/components/pantallaEntrada.vue
@@ -1,65 +1,89 @@
 <template>
- <!-- App.vue -->
-<v-app>
-  <v-app-bar app
-  color="green">
-  <h1 class="title">Universidad de Cordoba</h1>
-  </v-app-bar>
+<!-- App.vue -->
 
+<v-app>
   <!-- Sizes your content based upon application components -->
-  <v-main>
+  <v-main class="fondo">
 
     <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-
+    <v-container>
+    <br><br>
+    <v-row no-gutters>
+      <v-col align="center">
+        <input type="button"/>
+        <div class="content">
+          <router-link :to="{ name: 'contenidos' }" class="text-decoration-none routernone">
+            <v-img class="play mt-10 px-n12" src="../assets/logo.png.png"></v-img> 
+          </router-link>
+          <div class="sombra"></div>
+          <BR></BR>
+          <h1 class="title">TIPOS DE DATOS</h1>
+        </div>
+      </v-col>
+    </v-row>
   
-  <v-carousel>
-    <v-carousel-item
-      v-for="(item,i) in items"
-      :key="i"
-      :src="item.src"
-      reverse-transition="fade-transition"
-      transition="fade-transition"
-    ></v-carousel-item>
-  </v-carousel>
-
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
+  </v-container>
   </v-main>
 
-  <v-footer app>
-    <!-- -->
-  </v-footer>
-</v-app> 
+
+</v-app>
+
+
 </template>
 
 <style>
-.title{
-  color: aliceblue;
-}
+
+        .fondo{
+          background-image: url(../assets/background.png)
+          
+        }
+          .title{
+            color: white;
+          }
+        .content{
+            width: 100%;
+        }
+
+        .play{
+            width: 290px;
+            background-size: cover;
+            animation: animar  1s infinite alternate;
+            }
+
+        .sombra{
+            height: 100px;
+            width: 300px;
+            background:rgb(168, 168, 168);
+            border-radius: 50%;
+            transform: rotatex(-80deg);
+            animation: sombra 1s infinite alternate;
+        }
+
+        @keyframes animar{
+            to{
+                transform: translateY(-50px)
+            } 
+        }
+
+        @keyframes sombra{
+            to{
+                transform: scale(0.6) rotateX(-70deg);
+                opacity: 0.2;
+            }
+        }
+        
+      
 </style>
 
+
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
-      }
+export default {
+  name: "PantallaEntrada",
+  data: () => ({
+
+  }),
+
+  beforeDestroy() {
     },
-  }
+};
 </script>
