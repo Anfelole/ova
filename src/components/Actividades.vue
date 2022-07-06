7c266abd96a382dede17326081cf9d69af60a491
Anfelole <56459371+Anfelole@users.noreply.github.com>
2022-07-06 12:44:43 -0500

    ova
    andres h5p added

--- src/components/Actividades.vue
+++ src/components/Actividades.vue
@@ -1,18 +1,33 @@
 <template>
-<div>
-  <pagina-view><h1>Aqui van las actividades de tu pagina</h1></pagina-view>
-  
+<pagina-view>
+<div id="h5p-actividad_1">
+<p class="descripcion">Arrastra las palabras segun lo que se deseche en cada caneca</p>
 </div>
+</pagina-view>
 </template>
 
 <script>
 import PaginaView from "@/views/PaginaView.vue";
+import { H5P } from 'h5p-standalone';
 
 export default {
+     data: () => ({
+        
+    }),
   name: 'actividades',
 
   components: {
     PaginaView
-}
+},
+
+  mounted(){
+        const el = document.getElementById('h5p-actividad_1');
+        const options = {
+            h5pJsonPath: "../actividades/for",
+            frameJs: "../actividades/profe/frame.bundle.js",
+            frameCss: "../actividades/styles/h5p.css",
+        };
+        new H5P(el, options);
+    },
 }
 </script>
